dd8b575965e6249026e24f9e5f4efd922b915193
nikolarobottesla <13294739+nikolarobottesla@users.noreply.github.com>
2024-05-12 22:55:53 -0500

    shialt: enable nvidia powerManagement

--- hosts/shialt/hardware-configuration.nix
+++ hosts/shialt/hardware-configuration.nix
@@ -51,7 +51,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;  # when true seems to play in game videos better
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -72,6 +72,7 @@
 
     prime = {
       sync.enable = true;
+      # reverseSync.enable = true;  # seemed to be worse graphically, steam won't show GUI
       # Make sure to use the correct Bus ID values for your system!
       # sudo lshw -c display
       intelBusId = "PCI:0:2:0";
