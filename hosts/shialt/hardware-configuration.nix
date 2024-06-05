3d3045987423065e8d718dea436e1fed1c1c2ab5
nikolarobottesla <13294739+nikolarobottesla@users.noreply.github.com>
2024-06-04 22:20:02 -0500

    shialt: comment in nvidia

--- hosts/shialt/hardware-configuration.nix
+++ hosts/shialt/hardware-configuration.nix
@@ -39,44 +39,44 @@
     driSupport32Bit = true;  # default is false, only supported by nvidia/mesa
   };
 
-  # # Load nvidia driver for Xorg and Wayland
-  # services.xserver.videoDrivers = ["nvidia"];
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
 
-  # hardware.nvidia = {
+  hardware.nvidia = {
 
-  #   # Modesetting is required.
-  #   modesetting.enable = true;
+    # Modesetting is required.
+    modesetting.enable = true;
 
-  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-  #   # Enable this if you have graphical corruption issues or application crashes after waking
-  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-  #   # of just the bare essentials.
-  #   powerManagement.enable = true;  # when true seems to play in game videos better
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = true;  # when true seems to play in game videos better
 
-  #   # Fine-grained power management. Turns off GPU when not in use.
-  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #   powerManagement.finegrained = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
 
-  #   # Use the NVidia open source kernel module (not to be confused with the
-  #   # independent third-party "nouveau" open source driver).
-  #   # Support is limited to the Turing and later architectures. Full list of
-  #   # supported GPUs is at:
-  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-  #   # Only available from driver 515.43.04+
-  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
-  #   open = false;
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
 
-  #   # Enable the Nvidia settings menu,
-	#   # accessible via `nvidia-settings`.
-  #   nvidiaSettings = true;
+    # Enable the Nvidia settings menu,
+	  # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
 
-  #   prime = {
-  #     sync.enable = true;
-  #     # reverseSync.enable = true;  # seemed to be worse graphically, steam won't show GUI
-  #     # Make sure to use the correct Bus ID values for your system!
-  #     # sudo lshw -c display
-  #     intelBusId = "PCI:0:2:0";
-  #     nvidiaBusId = "PCI:1:0:0";
-  #   };
-  # };
+    prime = {
+      sync.enable = true;
+      # reverseSync.enable = true;  # seemed to be worse graphically, steam won't show GUI
+      # Make sure to use the correct Bus ID values for your system!
+      # sudo lshw -c display
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 }
