95b469dedf9169e53b851a395bbc7f902d4906fd
nikolarobottesla <13294739+nikolarobottesla@users.noreply.github.com>
2025-07-07 17:37:07 -0500

    update nextcloud

--- modules/nixos/nextcloud.nix
+++ modules/nixos/nextcloud.nix
@@ -5,7 +5,7 @@
   config,
   ...
 }: let
-  nextcloud_package = pkgs.nextcloud30;
+  nextcloud_package = pkgs.nextcloud31;
   cfg = config.my.nextcloud;
   inherit (lib) mkOption mkEnableOption types mkIf;
 in {
